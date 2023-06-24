a895bbda28a13bf7546f88248735d40861425d15
abehera01 <abehera920@gmail.com>
2023-06-24 23:25:43 +0530

    add jwt token

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('./db/config');
 const cors = require("cors");
 const express = require('express');
-const Jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const jwtKey = 'e-comm'
 const app =express();
 
@@ -20,7 +20,7 @@ app.post("/register",async(req,res)=>{
   newUser = newUser.toObject();
   delete newUser.password;
   if (newUser) {
-    Jwt.sign({ newUser }, jwtKey, { expiresIn: "2h" }, (err, token) => {
+    jwt.sign({ newUser }, jwtKey, { expiresIn: "2h" }, (err, token) => {
       if (err) {
         res.json({newUser: "Something went wrong, Please try after some time"});
       }
@@ -34,7 +34,7 @@ app.post("/login", async(req,res)=>{
   if(req.body.email && req.body.password){
     const user = await User.findOne(req.body).select("-password");
     if(user){
-      Jwt.sign({user}, jwtKey, {expiresIn: "2h"}, (err, token) =>{
+      jwt.sign({user}, jwtKey, {expiresIn: "2h"}, (err, token) =>{
         if(err){
           res.json({user:"Something went wrong, Please try after some time"})
         }
@@ -53,14 +53,14 @@ app.post("/login", async(req,res)=>{
 
 // Product
 // Insert Product
-app.post("/", async (req, res) => {
+app.post("/",verifyToken, async (req, res) => {
   const data = new Product(req.body);
   let result = await data.save();
   res.json(result);
 });
 
 // Get Product
-app.get("/:id", async (req, res) => {
+app.get("/:id",verifyToken, async (req, res) => {
   const id = req.params.id;
   const data = await Product.find({
     userId: { $all: [id] },
@@ -70,14 +70,14 @@ app.get("/:id", async (req, res) => {
 
 
 // Delete Product
-app.delete("/:id", async (req, res) => {
+app.delete("/:id",verifyToken, async (req, res) => {
   const id = req.params.id;
   const result = await Product.deleteOne({_id:id});
   res.json(result);
 });
 
 // Update Product
-app.put("/:id", async(req, res) => {
+app.put("/:id",verifyToken, async(req, res) => {
   const result = await Product.updateOne({ _id: req.params.id }, { $set: req.body });
   res.json(result);
 });
@@ -95,21 +95,51 @@ app.get("/search/:key", verifyToken, async (req, res) => {
 });
 
 // verify token
-function verifyToken(req, res, next) {
-  let token = req.headers["authorization"];
+async function verifyToken(req, res, next){
+  const token =await req.headers["authorization"];
   if (token) {
-    token = token.split(" ")[1];
-    Jwt.verify(token, jwtKey, (err, valid) => {
-      if (err) {
-        res.status(401).json({ result: "Please provide valid token" });
-      } else {
-        next();
-      }
-    });
-  } else {
-    res.status(403).json({ result: "Please add token with header" });
-  }
+     const authToken = token.split(" ")[1];
+     jwt.verify(authToken, jwtKey, (err, valid) => {
+       if (err) {
+         res.status(401).json({ result: "Please provide valid token" });
+     } else {
+       next();
+     }
+   });
+   } else {
+     res.status(403).json({ result: "Please add token with header" });
+ }
   next();
 }
 
+
+
 app.listen(8080);
+
+
+
+
+
+
+
+
+
+
+// const createToken = async () => {
+//   const token = await jwt.sign(
+//     { _id: "assxasa" },
+//     "aswedfrgtbvcdsewffdgthtbfgbffggfbfvdv",
+//     {
+//       expiresIn: "2h",
+//     }
+//   );
+//   console.log(token);
+
+//   const userVer = await jwt.verify(
+//     token,
+//     "aswedfrgtbvcdsewffdgthtbfgbffggfbfvdv"
+//   );
+//   console.log(userVer);
+// };
+
+// createToken();
